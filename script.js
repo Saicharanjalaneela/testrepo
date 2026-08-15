4537869cf4d847a5ce85773b59f1f2db70de03e7
sai charan jalaneela <saicharanjalaneela@gmail.com>
2026-08-15 10:21:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.write("<ol>");
+document.write("<li>Web Technologies</li>");
+document.write("<li>Java Programming</li>");
+document.write("<li>Database Management System</li>");
+document.write("<li>Operating Systems</li>");
+document.write("<li>Digital Proficiency</li>");
+document.write("</ol>");
